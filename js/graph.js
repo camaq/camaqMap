89a1406ba1728966ccd705c64e9e759ad53a0898
camaq <sg@camaq.org>
2017-06-06 13:07:53 -0400

    Update graph.js

--- js/graph.js
+++ js/graph.js
@@ -969,7 +969,7 @@ graphProvincial = function () { return {
 						
 		{"id": 8, "titre": "Emploi-Québec", "graphOpen": "NAN", "symb": ["S"],
 				"mission": "Emploi-Québec offre différentes mesures d’aide à l’emploi destinées aux individus et aux entreprises, dont du soutien à la formation, afin de favoriser l’intégration et le maintien en emploi.<br><br>",
-				"activite": "<span style=\"font-weight: bold;\"></span><br><br>\
+				"activite": "<span style=\"font-weight: bold;\">test</span><br><br>\
  							\u27E9 Pour trouver le centre local d’emploi ou le bureau de Services Québec le plus près de chez vous : www.mess.gouv.qc.ca/services-en-ligne/centres-locaux-emploi/localisateur/services.asp",
 				"lien":  "http://www.emploiquebec.gouv.qc.ca" },
 				
